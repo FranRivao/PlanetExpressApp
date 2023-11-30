5ff47c4217ffe4c7df67a39ba842e44f6fde644d
sanmildon <alexsanmil@gmail.con>
2023-11-30 18:43:44 +0100

    30/11

--- src/Envio.java
+++ src/Envio.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  *
  * @author
  * @author
- * @version     1.0
+ * @version 1.0
  */
 public class Envio {
 
@@ -37,32 +37,44 @@ public class Envio {
         this.columna = columna;
         this.precio = precio;
     }
+
     public String getLocalizador() {
         return localizador;
     }
+
     public Porte getPorte() {
         return porte;
     }
+
     public Cliente getCliente() {
         return cliente;
     }
+
     public int getFila() {
         return fila;
     }
+
     public int getColumna() {
         return columna;
     }
+
     // TODO: Ejemplos: "1A" para el hueco con fila 1 y columna 1, "3D" para el hueco con fila 3 y columna 4
     public String getHueco() {
-        return "";
+        return String.valueOf(fila) + 'A' + (columna - 1);
     }
+
     public double getPrecio() {
         return precio;
     }
+
     //TODO: Texto que debe generar: Envío PM1111AAAABBBBC para Porte PM0066 de GGT M5 (01/01/2023 08:15:00) a CID M1 (01/01/2024 11:00:05) en hueco 6C por 13424,56 SSD
     public String toString() {
+        return "Envío "+localizador+" para Porte "+porte.getID()+" de "
+                +porte.getOrigen().getCodigo()+" ("+porte.getSalida().toString()+") a "+porte.getDestino().getCodigo()
+                +" ("+porte.getLlegada().toString()+") en hueco "+getHueco()+" por "+precio+" SSD ";
 
     }
+
     // TODO: Cancela este envío, eliminándolo de la lista de envíos del porte y del cliente correspondiente
     public boolean cancelar() {
 
@@ -71,28 +83,23 @@ public class Envio {
     /**
      * TODO: Método para imprimir la información de este envío en un fichero que respecta el formato descrito en el
      *  enunciado
+     *
      * @param fichero
      * @return Devuelve la información con el siguiente formato como ejemplo ->
-     *     -----------------------------------------------------
-     *     --------- Factura del envío PM1111AAAABBBBC ---------
-     *     -----------------------------------------------------
-     *     Porte: PM0066
-     *     Origen: Gaia Galactic Terminal (GGT) M5
-     *     Destino: Cidonia (CID) M1
-     *     Salida: 01/01/2023 08:15:00
-     *     Llegada: 01/01/2024 11:00:05
-     *     Cliente: Zapp Brannigan, zapp.brannigan@dop.gov
-     *     Hueco: 6C
-     *     Precio: 13424,56 SSD
+     * -----------------------------------------------------
+     * --------- Factura del envío PM1111AAAABBBBC ---------
+     * -----------------------------------------------------
+     * Porte: PM0066
+     * Origen: Gaia Galactic Terminal (GGT) M5
+     * Destino: Cidonia (CID) M1
+     * Salida: 01/01/2023 08:15:00
+     * Llegada: 01/01/2024 11:00:05
+     * Cliente: Zapp Brannigan, zapp.brannigan@dop.gov
+     * Hueco: 6C
+     * Precio: 13424,56 SSD
      */
     public boolean generarFactura(String fichero) {
         try {
-
-
-
-
-
-
 
 
             return true;
@@ -102,18 +109,17 @@ public class Envio {
     }
 
 
-
     /**
-     *	TODO: Genera un localizador de envío. Este consistirá en una cadena de 15 caracteres, de los cuales los seis
-	 *   primeros será el ID del porte asociado y los 9 siguientes serán letras mayúsculas aleatorias. Ejemplo: PM0123ABCD
+     * TODO: Genera un localizador de envío. Este consistirá en una cadena de 15 caracteres, de los cuales los seis
+     *   primeros será el ID del porte asociado y los 9 siguientes serán letras mayúsculas aleatorias. Ejemplo: PM0123ABCD
      *   NOTA: Usar el objeto rand pasado como argumento para la parte aleatoria.
+     *
      * @param rand
      * @param idPorte
      * @return
      */
     public static String generarLocalizador(Random rand, String idPorte) {
         StringBuilder localizador = new StringBuilder(idPorte);
-
 
 
         return localizador.toString();
@@ -124,6 +130,7 @@ public class Envio {
      * TODO: Método para crear un nuevo envío para un porte y cliente específico, pidiendo por teclado los datos
      *  necesarios al usuario en el orden y con los textos indicados en los ejemplos de ejecución del enunciado
      *  La función solicita repetidamente los parámetros hasta que sean correctos
+     *
      * @param teclado
      * @param rand
      * @param porte
@@ -133,7 +140,6 @@ public class Envio {
     public static Envio altaEnvio(Scanner teclado, Random rand, Porte porte, Cliente cliente) {
 
 
-
-        return ;
+        return;
     }
 }
--- src/ListaEnvios.java
+++ src/ListaEnvios.java
@@ -9,103 +9,160 @@ import java.util.Scanner;
  *
  * @author
  * @author
- * @version     1.0
+ * @version 1.0
  */
 public class ListaEnvios {
     private Envio[] envios;
+
     /**
      * TODO: Constructor de la clase para inicializar la lista a una capacidad determinada
      *
      * @param capacidad
      */
     public ListaEnvios(int capacidad) {
-		
-		
+        envios = new Envio[capacidad];
+
+
     }
+
     // TODO: Devuelve el número de envíos que hay en la lista
     public int getOcupacion() {
+        int pos = 0;
+        while (envios[pos] != null) {
+            pos++;
+        }
+        return pos;
 
     }
+
     // TODO: ¿Está llena la lista de envíos?
     public boolean estaLlena() {
+        return envios.length == getOcupacion();
+
 
     }
-	//TODO: Devuelve el envio dado un indice
+
+    //TODO: Devuelve el envio dado un indice
     public Envio getEnvio(int i) {
-        return null;
+
+        return envios[i];
     }
 
     /**
      * TODO: insertamos un nuevo envío en la lista
+     *
      * @param envio
      * @return true en caso de que se añada correctamente, false en caso de lista llena o error
      */
     public boolean insertarEnvio(Envio envio) {
-
-        return false;
+        boolean res = false;
+        if (!estaLlena()) {
+            int pos = 0;
+            while (envios[pos] != null) {
+                pos++;
+            }
+            envios[pos] = envio;
+            res = true;
+        } else res = false;
+        return res;
     }
 
     /**
      * TODO: Buscamos el envio a partir del localizador pasado como parámetro
+     *
      * @param localizador
      * @return el envio que encontramos o null si no existe
      */
     public Envio buscarEnvio(String localizador) {
+        int pos = 0;
+        Envio envio = null;
+        while (envios[pos] != null && envio == null) {
+            if (envios[pos].getLocalizador() == localizador) {
+                envio = envios[pos];
+            }
+            pos++;
+        }
+        return envio;
 
 
-        return null;
     }
 
     /**
      * TODO: Buscamos el envio a partir del idPorte, fila y columna pasados como parámetros
+     *
      * @param idPorte
      * @param fila
      * @param columna
      * @return el envio que encontramos o null si no existe
      */
+
     public Envio buscarEnvio(String idPorte, int fila, int columna) {
-
-
-        return null;
+        int pos = 0;
+        Envio envio = null;
+        while (envios[pos] != null && envio == null) {
+            if (envios[pos].getPorte().getID() == idPorte && envios[pos].getFila() == fila && envios[pos].getColumna() == columna) {
+                envio = envios[pos];
+            }
+            pos++;
+        }
+        return envio;
     }
 
     /**
      * TODO: Eliminamos un envio a través del localizador pasado por parámetro
+     *
      * @param localizador
      * @return True si se ha borrado correctamente, false en cualquier otro caso
      */
-    public boolean eliminarEnvio (String localizador) {
-
-        return false;
+    public boolean eliminarEnvio(String localizador) {
+        boolean res = true;
+        int pos = 0;
+        while (pos < envios.length && envios[pos].getLocalizador() != localizador) {
+            pos++;
+        }
+        if (pos == envios.length) {
+            res = false;
+        } else {
+            for (int i = pos; i < envios.length - 1; i++) {
+                envios[i] = envios[i + 1];
+            }
+            envios[envios.length - 1] = null;
+        }
+        return res;
     }
 
     /**
      * TODO: Muestra por pantalla los Envios de la lista, con el formato que aparece
      * en el enunciado
      */
-    public void listarEnvios() {
 
+
+    public void listarEnvios() {
+        for (int i = 0; i < envios.length; i++) {
+            System.out.println(envios[i].toString());
+        }
     }
 
     /**
      * TODO: Permite seleccionar un Envio existente a partir de su localizador, usando el mensaje pasado como argumento
      *  para la solicitud y siguiendo el orden y los textos mostrados en el enunciado.
      *  La función solicita repetidamente hasta que se introduzca un localizador correcto
+     *
      * @param teclado
      * @param mensaje
      * @return
      */
     public Envio seleccionarEnvio(Scanner teclado, String mensaje) {
-        Envio envio = null;
-
-
+        ;
+        System.out.println(mensaje);
+        Envio envio = envios[teclado.nextInt()];
         return envio;
     }
 
 
-
     /**
      * TODO: Añade los Envios al final de un fichero CSV, SIN SOBREESCRIBIR la información
+     *
      * @param fichero
      * @return
      */
@@ -123,6 +180,7 @@ public class ListaEnvios {
 
     /**
      * TODO: Lee los Envios del fichero CSV y los añade a las listas de sus respectivos Portes y Clientes
+     *
      * @param ficheroEnvios
      * @param portes
      * @param clientes
