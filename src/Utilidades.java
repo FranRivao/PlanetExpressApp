af46671177fd3370a0ba4e98ca3ee3ad615a41d6
FranRivao <franrivao2.0@gmail.com>
2023-12-12 20:27:59 +0100

    Utilidades

--- src/Utilidades.java
+++ src/Utilidades.java
@@ -158,9 +158,4 @@ public class Utilidades {
         System.out.print(s);
         return teclado.next();
     }
-
-    public static char[] getLetras() {
-        char letras[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-        return letras;
-    }
 }
